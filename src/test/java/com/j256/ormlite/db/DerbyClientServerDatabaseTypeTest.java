42ca29c8a37a343cec73dc38525aff368cfb34f1
Bo Anderson <mail@boanderson.me>
2018-12-17 12:36:05 +0000

    Adjusted indication of nested savepoints support in some DBs.

--- src/test/java/com/j256/ormlite/db/DerbyClientServerDatabaseTypeTest.java
+++ src/test/java/com/j256/ormlite/db/DerbyClientServerDatabaseTypeTest.java
@@ -4,9 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 public class DerbyClientServerDatabaseTypeTest extends DerbyEmbeddedDatabaseTypeTest {
+
+	@Override
+	protected void setDatabaseParams() throws SQLException {
+		databaseUrl = "jdbc:derby://localhost:1527/MyDbTest;create=true";
+		connectionSource = new JdbcConnectionSource(DEFAULT_DATABASE_URL);
+		databaseType = new DerbyClientServerDatabaseType();
+	}
 
 	@Test
 	public void testGetClientServerDriverClassName() {
@@ -16,7 +26,12 @@ public class DerbyClientServerDatabaseTypeTest extends DerbyEmbeddedDatabaseType
 	@Test
 	public void testIsDatabaseUrlThisType() {
 		assertTrue(new DerbyClientServerDatabaseType()
-				.isDatabaseUrlThisType("jdbc:derby://localhost:1527/MyDbTest;create=true';", "derby"));
+				.isDatabaseUrlThisType("jdbc:derby://localhost:1527/MyDbTest;create=true", "derby"));
 		assertFalse(new DerbyClientServerDatabaseType().isDatabaseUrlThisType("jdbc:derby:database", "derby"));
+	}
+
+	@Test
+	public void testIsNestedSavePointsSupported() {
+		assertFalse(databaseType.isNestedSavePointsSupported());
 	}
 }
